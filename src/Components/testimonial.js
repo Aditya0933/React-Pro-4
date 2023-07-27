f7e67eccd3a620b9d83eb662a0b752d4704f1025
Aditya Singh Parihar <201037@kit.ac.in>
2023-07-27 13:46:50 +0530

    Initial Commit

---
+++ src/Components/testimonial.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import Card from "./Card";
+import {FiChevronLeft,FiChevronRight } from 'react-icons/fi'
+const Testimonial =(propes)=>{
+    let review = propes.review
+    const[index, setIndex] = useState(0)
+    const leftShiftHandler =()=> {
+        if(index-1 < 0){
+            setIndex(review.length - 1);
+        }
+        else{
+            setIndex(index-1)
+        }
+    }
+    const rightShiftHandler =()=> {
+        if(index+1 >= review.length){
+            setIndex(0);
+        }
+        else{
+            setIndex(index+1)
+        }
+    }
+    const surpriseShiftHandler =()=> {
+        let randomIndex = Math.floor(Math.random()*review.length);
+        setIndex(randomIndex)
+    }
+    return(
+        <div>
+            <Card review={review[index]}></Card>
+            <div className='text-center flex text-3xl mt-5 gap-3 text-violet-400 font-bold'>
+                <button onClick={leftShiftHandler} className='cursor-pointer hover:text-violet-500'><FiChevronLeft/></button>
+                <button onClick={rightShiftHandler} className='cursor-pointer hover:text-violet-500'><FiChevronRight/></button>
+            </div>
+            <div>
+                <button onClick={surpriseShiftHandler} className='bg-violet-400 hover:bg-violet-600 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg'>Surprise Me</button>
+            </div>
+        </div>
+
+    )
+}
+export default Testimonial;
---
+++ src/data.js
@@ -0,0 +1,38 @@
+const review = [
+    {
+        id:1,
+        name:"Aditya Singh Parihar",
+        job:"Software Engineer",
+        image:"https://images.unsplash.com/photo-1619279302118-43033660826a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHN0dWRlbnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+        text:"jhEbfvwerhbvwrvbrjwrvbweJHVbrjvb vjns vjnv wrbv <VJS vJS<VH vj Vjld vvhj vasjvh jlva vvjsV Sfjvaf v,nj VFVJLF  vjnv wrbv <VJS vJS<VH vj Vjld vvhj vasjvh jlva vvjsV SfjvafvVFVJLFVvjhasjhvbjbjwhbvrwvbvbvjlbviuebvevbajhjvabvubvhjjsvkjvjvbsvjbvjvkjvbrwhjrvbbjdgsgyrruggugufgtfugbgrwejsfbfrjrebbrejhbejabhrjrglrgpegbjehbgjrereubgrjbhjvhbhjbverbrvbv  jrhbrblgb erhgbghjel hj erh er rghjg jh hg r  jh rjarljrl ljsghbvjhblvruierigheugighiugVv"
+    },
+    {
+        id:2,
+        name:"Aditya Singh ",
+        job:"Software Engineer",
+        image:"https://images.unsplash.com/photo-1619279302118-43033660826a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHN0dWRlbnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+        text:"jhEbfvwerhbvwrvbrjwrvbweJHVbrjvb vjns vjnv wrbv <VJS vJS<VH vj Vjld vvhj vasjvh jlva vvjsV SfjvafvVFVJLFVvjhasjhvbjbjwhbvrwvbvbvjlbviuebvevbajhjvabvubvhjjsvkjvjvbsvjbvjvkjvbrwhjrvbbjdgsgyrruggugufgtfugbgrwejsfbfrjrebbrejhbejabhrjrglrgpegbjehbgjrereubgrjbhjvhbhjbverbrvbv  jrhbrblgb erhgbghjel hj erh er rghjg jh hg r  jh rjarljrl ljsghbvjhblvruierigheugighiug"
+    },
+    {
+        id:3,
+        name:"Aditya",
+        job:"Software Engineer",
+        image:"https://images.unsplash.com/photo-1619279302118-43033660826a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHN0dWRlbnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+        text:"jhEbfvwerhbvwrvbrjwrvbweJHVbrjvb vjns vjnv wrbv <VJS vJS<VH vj Vjld vvhj vasjvh jlva vvjsV Sfjvaf v,nj VFVJLF  vjnv wrbv <VJS vJS<VH vj Vjld vvhj vasjvh jlva vvjsV SfjvafvVFVJLFVvjhasjhvbjbjwhbvrwvbvbvjlbviuebvevbajhjvabvubvhjjsvkjvjvbsvjbvjvkjvbrwhjrvbbjdgsgyrruggugufgtfugbgrwejsfbfrjrebbrejhbejabhrjrglrgpegbjehbgjrereubgrjbhjvhbhjbverbrvbv  jrhbrblgb erhgbghjel hj erh er rghjg jh hg r  jh rjarljrl ljsghbvjhblvruierigheugighiugVv"
+    },
+    {
+        id:4,
+        name:"Aadi",
+        job:"Software Engineer",
+        image:"https://images.unsplash.com/photo-1619279302118-43033660826a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHN0dWRlbnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+        text:"jhEbfvw erhbvwrvbrjwrvbweJHVbrjvb vjns vjnv wrbv <VJS vJS<VH vj Vjld vvhj vasjvh jlva vvjsV Sfjvaf v,nj VFvjhasjhvbjbjwhbvrwvbvbvjlbviuebvevbajhjvabvubvhjjsvkjvjvbsvjbvjvkjvbrwhjrvbbjdgsgyrruggugufgtfugbgrwejsfbfrjrebbrejhbejabhrjrglrgpegbjehbgjrereubgrjbhjvhbhjbverbrvbv  jrhbrblgb erhgbghjel hj erh er rghjg jh hVJLFVv"
+    },
+    {
+        id:5,
+        name:"A",
+        job:"Software Engineer",
+        image:"https://images.unsplash.com/photo-1619279302118-43033660826a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHN0dWRlbnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+        text:"jhEbfvwerhbvwrvbrjwrvbweJHVbrjvb vjns vjnv wrbv <VJS vJS<VH vj Vjld vvhj vasjvh jlva vvjsV Sfjvaf v,nj VFvjhasjhvbjbjwhbvrwvbvbvjlbviuebvevbajhjvabvubvhjjsvkjvjvbsvjbvjvkjvbrwhjrvbbjdgsgyrruggugufgtfugbgrwejsfbfrjrebbrejhbejabhrjrglrgpegbjehbgjrereubgrjbhjvhbhjbverbrvbv  jrhbrblgb erhgbghjel hj erh er rghjg jh hVJLF Vv",
+    }
+];
+export default review;
