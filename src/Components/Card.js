cebb4623ae07087edf2cf31ad05f78a3e01f7615
Aditya Singh Parihar <201037@kit.ac.in>
2023-07-29 11:04:18 +0530

    Responsive

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,14 +4,12 @@ const Card =(props)=>{
     return(
         <div className='flex flex-col md:relative'>
             <div className='absolute top-[-7rem] z-[10] mx-auto'>
-                <img className='aspect-square rounded-full w-[140px] h-[140px] z[25]' src={review.image}></img>
-                <div className='aspect-square rounded-full w-[140px] h-[140px] z[-10] bg-black absolute top-[-6px] left-[-6px]'></div>
+                <img className='aspect-square rounded-full w-[140px] h-[140px] z-[100]' src={review.image}></img>
+                {/* <div className='aspect-square rounded-full w-[140px] h-[140px] bg-black absolute top-[-6px] right-[-6px]'></div> */}
             </div>
             <div className='text-center mt-7'>
-                <p className='font-bold text-2xl'>{review.name}</p>               
-            </div>
-            <div className='text-center mt-7'>
-                <p className='text-violet-300 uppercase text-sm'>{review.job}</p>
+                <p className='tracking-wider font-bold text-2xl'>{review.name}</p>       
+                <p className='text-violet-300 uppercase text-sm'>{review.job}</p>        
             </div>
             <div className='text-center mt-5 text-violet-300 mx-auto'>
                 <FaQuoteLeft/>
--- src/Components/testimonial.js
+++ src/Components/testimonial.js
@@ -25,13 +25,13 @@ const Testimonial =(propes)=>{
         setIndex(randomIndex)
     }
     return(
-        <div>
+        <div className="w-[85vw] md-[700px] bg-white flex flex-col justify-center items-center mt-10 pd-10 transition-all duration-700 hover:shadow-xl rounded-md ">
             <Card review={review[index]}></Card>
-            <div className='text-center flex text-3xl mt-5 gap-3 text-violet-400 font-bold'>
+            <div className='text-center flex text-3xl mt-10 gap-3 text-violet-400 font-bold'>
                 <button onClick={leftShiftHandler} className='cursor-pointer hover:text-violet-500'><FiChevronLeft/></button>
                 <button onClick={rightShiftHandler} className='cursor-pointer hover:text-violet-500'><FiChevronRight/></button>
             </div>
-            <div>
+            <div className="mt-5">
                 <button onClick={surpriseShiftHandler} className='bg-violet-400 hover:bg-violet-600 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg'>Surprise Me</button>
             </div>
         </div>
